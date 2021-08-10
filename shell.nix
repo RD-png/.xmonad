8cc0d0e014f2bf828eca7eb6c9f06fc603d4cec6
Ryan Denby <ryan.543@live.co.uk>
2021-08-10 17:51:50 +0100

    Added enviroment for editing xmonad

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8104" }:
+let
+  inherit (nixpkgs) pkgs;
+  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
+    xmonad
+    xmonad-contrib
+    xmonad-extras
+  ]);
+in
+  pkgs.stdenv.mkDerivation {
+    name = "my-haskell-env-0";
+    buildInputs = [ ghc ];
+    shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+  }
